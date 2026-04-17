d6635d46825a94dc4dd8ac5aeab82b0002f78453
Garima <yadavgarima451@gmail.com>
2026-04-17 17:25:59 +0530

    path changes

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,9 +1,15 @@
 import { io } from "socket.io-client";
 
 // Explicitly add port 3000
-const socket = io("http://13.60.253.32", {
-  withCredentials: true,
-  transports: ["websocket"] // Helps bypass some proxy issues
-});
+// const socket = io("http://13.60.253.32", {
+//   withCredentials: true,
+//   transports: ["websocket"] // Helps bypass some proxy issues
+// });
 
-export default socket;
+// export default socket;
+
+const socketInstance = io(Socket_Url, {
+  withCredentials: true,
+  transports: ["websocket"], // Force WebSocket only
+  path: "/socket.io/"
+});
