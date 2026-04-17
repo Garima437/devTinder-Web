a8ae97742e1fee3635879be5253937d8ee312aee
Garima <yadavgarima451@gmail.com>
2026-04-17 17:13:41 +0530

    remove polling

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // Explicitly add port 3000
 const socket = io("http://13.60.253.32", {
   withCredentials: true,
-  transports: ["websocket", "polling"] // Helps bypass some proxy issues
+  transports: ["websocket"] // Helps bypass some proxy issues
 });
 
 export default socket;
