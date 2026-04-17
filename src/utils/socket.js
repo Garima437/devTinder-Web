5d579b93d23913c0c96227a45e5e816bb6b16fdb
Garima <yadavgarima451@gmail.com>
2026-04-17 16:44:10 +0530

    port change in socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Explicitly add port 3000
-const socket = io("http://13.60.253.32:3000", {
+const socket = io("http://13.60.253.32", {
   withCredentials: true,
   transports: ["websocket", "polling"] // Helps bypass some proxy issues
 });
